6f889128f65efa73e5999939c075ebd70c1fbea4
Tomassous-38 <57865102+Tomassous-38@users.noreply.github.com>
2023-08-02 00:00:54 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,8 @@ from langchain import OpenAI
 import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-def fetch_results(api_key, keyword, location="Paris, Paris, Ile-de-France, France"):
+def fetch_results(api_key, keyword, debug=False, location="Paris, Paris, Ile-de-France, France"):
+    if debug: st.write("Fetching results with keyword:", keyword)
     params = {
         "api_key": api_key,
         "engine": "google",
@@ -18,44 +19,52 @@ def fetch_results(api_key, keyword, location="Paris, Paris, Ile-de-France, Franc
         "gl": "fr",
         "hl": "fr"
     }
+    if debug: st.write("Search parameters:", params)
     search = GoogleSearch(params)
     results = search.get_dict()
-    time.sleep(4)
-    return [item['link'] for item in results['organic_results'][:5]]
+    urls = [item['link'] for item in results['organic_results'][:5]]
+    if debug: st.write("Fetched URLs:", urls)
+    return urls
 
-def get_text_from_url(url):
+def get_text_from_url(url, debug=False):
+    if debug: st.write("Fetching content from URL:", url)
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    return soup.get_text()
+    text = soup.get_text()
+    if debug: st.write("Fetched text:", text)
+    return text
 
-def summarize_url_no_st(url, openai_api_key):
-    text = get_text_from_url(url)
+def summarize_url_no_st(url, openai_api_key, debug=False):
+    text = get_text_from_url(url, debug)
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=3000,
         chunk_overlap=200
     )
+    documents = text_splitter.create_documents([text])
+    if debug: st.write("Split documents:", documents)
     llm = OpenAI(openai_api_key=openai_api_key, temperature=0.8)
     chain_summarize = load_summarize_chain(llm, chain_type="map_reduce")
-    documents = text_splitter.create_documents([text])
     summaries = []
     for doc in documents:
         if isinstance(doc, tuple):
             doc = {'page_content': doc[0]}
+        if debug: st.write("Document to be summarized:", doc)
         summarized_texts = chain_summarize.run([doc])
+        if debug: st.write("Summarized texts:", summarized_texts)
         summaries.append(summarized_texts)
     return url, summaries
 
 def summarize_text(urls, openai_api_key, debug=False):
     summaries = []
     with ThreadPoolExecutor() as executor:
-        future_summaries = {executor.submit(summarize_url_no_st, url, openai_api_key): url for url in urls}
+        future_summaries = {executor.submit(summarize_url_no_st, url, openai_api_key, debug): url for url in urls}
         for future in as_completed(future_summaries):
             url, summary = future.result()
             summaries.append((url, summary))
             if debug:
                 st.write(f"Processed URL: {url}")
+                st.write(f"Summary: {summary}")
 
-    if debug: st.write("Summaries created.")
     return summaries
 
 def main():
@@ -66,7 +75,7 @@ def main():
     debug_mode = st.checkbox("Debug Mode")
 
     if st.button("Send") and api_key and keyword and openai_api_key:
-        urls = fetch_results(api_key, keyword)
+        urls = fetch_results(api_key, keyword, debug=debug_mode)
         st.write("Top 5 URLs:", urls)
         summaries = summarize_text(urls, openai_api_key, debug=debug_mode)
         for url, summary in summaries:
