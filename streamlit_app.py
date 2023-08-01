d289de1a73559a52a0a2aa4c64167d98fc3382f4
Tomassous-38 <57865102+Tomassous-38@users.noreply.github.com>
2023-08-01 21:34:55 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from serpapi import GoogleSearch
+from google_search_results import GoogleSearch
+import requests
+from bs4 import BeautifulSoup
 
 def fetch_results(api_key, keyword, engine="google", location="Paris, Ile-de-France, France"):
     params = {
@@ -13,9 +15,19 @@ def fetch_results(api_key, keyword, engine="google", location="Paris, Ile-de-Fra
     
     search = GoogleSearch(params)
     results = search.get_dict()
-    
     urls = [item['link'] for item in results['organic_results'][:5]]
-    return urls
+
+    contents = []
+    for url in urls:
+        try:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            content = soup.get_text()
+            contents.append(content)
+        except:
+            contents.append("Failed to fetch content.")
+
+    return urls, contents
 
 st.title("Google Top 5 URLs Scraper")
 st.write("Please enter your SERPapi key and the keyword.")
@@ -24,7 +36,8 @@ api_key = st.text_input("SERPapi Key")
 keyword = st.text_input("Keyword")
 
 if api_key and keyword:
-    urls = fetch_results(api_key, keyword)
+    urls, contents = fetch_results(api_key, keyword)
     st.write("Top 5 URLs:")
-    for url in urls:
+    for url, content in zip(urls, contents):
         st.write(url)
+        st.text_area("Content:", value=content, height=200)
