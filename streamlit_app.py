30516a901ecfcf8e433d7a1c95829074fa7eb6d5
Tomassous-38 <57865102+Tomassous-38@users.noreply.github.com>
2023-08-01 23:51:15 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
 from langchain import OpenAI
 import time
+from concurrent.futures import ThreadPoolExecutor
 
 def fetch_results(api_key, keyword, debug=False, location="Paris, Paris, Ile-de-France, France"):
     if debug: st.write("Fetching results...")
@@ -31,25 +32,28 @@ def get_text_from_url(url, debug=False):
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup.get_text()
 
-def summarize_text(urls, openai_api_key, debug=False):
+def summarize_url(url, openai_api_key, debug=False):
+    if debug: st.write(f"Processing URL: {url}")
+    text = get_text_from_url(url, debug=debug)
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=3000,
         chunk_overlap=200
     )
     llm = OpenAI(openai_api_key=openai_api_key, temperature=0.8)
     chain_summarize = load_summarize_chain(llm, chain_type="map_reduce")
-
+    documents = text_splitter.create_documents([text])
     summaries = []
-    for url in urls:
-        if debug: st.write(f"Processing URL: {url}")
-        text = get_text_from_url(url, debug=debug)
-        documents = text_splitter.create_documents([text])
-        for doc in documents:
-            if isinstance(doc, tuple):
-                doc = {'page_content': doc[0]}
-            summarized_texts = chain_summarize.run([doc])
-            summaries.append((url, summarized_texts))
+    for doc in documents:
+        if isinstance(doc, tuple):
+            doc = {'page_content': doc[0]}
+        summarized_texts = chain_summarize.run([doc])
+        summaries.append(summarized_texts)
+    return url, summaries
 
+def summarize_text(urls, openai_api_key, debug=False):
+    with ThreadPoolExecutor() as executor:
+        future_summaries = {executor.submit(summarize_url, url, openai_api_key, debug): url for url in urls}
+        summaries = [future.result() for future in concurrent.futures.as_completed(future_summaries)]
     if debug: st.write("Summaries created.")
     return summaries
 
